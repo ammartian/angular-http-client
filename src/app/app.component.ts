5dfa02daf02101ba5d6a88453a3e6ea385348724
ammartian <ammarhakimii1208@gmail.com>
2022-09-12 10:16:12 +0800

    Init commit and get POST

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Post } from './post';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-http-client';
+
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+
+  posts!: Observable<any>;
+
+  constructor(private http: HttpClient) {}
+
+  getPosts() {
+    let params = new HttpParams().set('userId', '1');
+    let headers = new HttpHeaders().set('Authorization', 'auth-token');
+
+    this.posts = this.http.get(this.ROOT_URL + '/posts', { params, headers })
+  }
+
 }
